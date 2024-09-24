1946060f6fe76ffb00b22c816eee67b1046070ac
Gaspar Arimany <gaspar.arimany@gmail.com>
2024-09-24 18:13:40 -0300

    chore: adds pool of words adds initial logic of selecting a random word adds button to reset game state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
+import { words } from './words';
+import { getRandomWord } from './utils';
 
 const AMOUNT_OF_GUESSES = 6;
 
@@ -11,9 +13,32 @@ function App() {
 }
 
 function Game() {
+  const [word, setWord] = useState(getRandomWord(words));
+  const [guesses, setGuesses] = useState<string[]>([]);
+
+  const usedLetters = useMemo(() => {
+    const letterSet = new Set();
+
+    const lastGuess = guesses[guesses.length - 1];
+    lastGuess.split('').forEach((letter) => {
+      letterSet.add(letter);
+    });
+
+    return;
+  }, [guesses]);
+
+  const handleReset = useCallback(() => {
+    setWord(getRandomWord(words));
+    setGuesses([]);
+  }, []);
+
   return (
     <section className='max-w-lg border-2 border-slate-100 my-0 mx-auto'>
       <Guesses />
+      <div className='flex flex-col gap-4'>
+        <button onClick={handleReset}>Reset the word</button>
+        <span className='text-zinc-200'>{word}</span>
+      </div>
     </section>
   );
 }
