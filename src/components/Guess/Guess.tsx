fd5008b73a5398c33dee3b1dd8461ad10001e90f
Denoph <gaspar.arimany@gmail.com>
2024-11-24 17:44:16 -0300

    chore: minor css adjustments in guess component
    npm audit fix

--- src/components/Guess/Guess.tsx
+++ src/components/Guess/Guess.tsx
@@ -1,20 +1,20 @@
-import { WORD_LENGTH } from '../../App';
+import { WORD_LENGTH } from "../../App";
 
-export function Guess({ guess }: { guess: string }) {
-  return (
-    <div className='flex justify-center gap-4'>
-      {[...Array.from({ length: WORD_LENGTH }, (_, index) => index + 1)].map(
-        (_: number, i) => {
-          return (
-            <div
-              key={i}
-              className='box-content text-center text-2xl font-bold py-3 px-3 min-h-6 min-w-6 border-2 border-white'
-            >
-              L
-            </div>
-          );
-        }
-      )}
-    </div>
-  );
+export function Guess({ guess = "" }: { guess: string }) {
+	return (
+		<div className="flex justify-center gap-4">
+			{[...Array.from({ length: WORD_LENGTH }, (_, index) => index + 1)].map(
+				(_: number, i) => {
+					return (
+						<div
+							key={i}
+							className="inline-block text-center text-2xl font-bold py-2 px-2 size-14 border-2 border-white"
+						>
+							<span className="align-middle">{guess.charAt(i)}</span>
+						</div>
+					);
+				}
+			)}
+		</div>
+	);
 }
