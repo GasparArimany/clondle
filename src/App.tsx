f37973e6f8156e4ceab63dcb5cc0ac7339f73b40
Denoph <gaspar.arimany@gmail.com>
2024-11-25 17:03:30 -0300

    chore: guess component improvement

--- src/App.tsx
+++ src/App.tsx
@@ -1,111 +1,111 @@
-import { useCallback, useMemo, useRef, useState } from 'react';
-import { words } from './words';
-import { getRandomWord } from './utils';
-import { Toast, ToastRef } from './components/Toast/Toast';
-import { Guess } from './components/Guess/Guess';
+import { useCallback, useMemo, useRef, useState } from "react";
+import { words } from "./words";
+import { getRandomWord } from "./utils";
+import { Toast, ToastRef } from "./components/Toast/Toast";
+import { Guess } from "./components/Guess/Guess";
 
 const AMOUNT_OF_GUESSES = 6;
 export const WORD_LENGTH = 5;
 
-export const MAIN_CONTENT_ID = 'wordle-main';
+export const MAIN_CONTENT_ID = "wordle-main";
 
 function App() {
-  return (
-    <main
-      id={MAIN_CONTENT_ID}
-      className='bg-gray-800 text-gray-200 h-screen w-full'
-    >
-      <Game />
-    </main>
-  );
+	return (
+		<main
+			id={MAIN_CONTENT_ID}
+			className="bg-gray-800 text-gray-200 h-screen w-full"
+		>
+			<Game />
+		</main>
+	);
 }
 
-type GameStatus = 'playing' | 'won' | 'lost';
+type GameStatus = "playing" | "won" | "lost";
 
 function Game() {
-  const [word, setWord] = useState(getRandomWord(words));
-  const [currentGuess, setCurrentGuess] = useState('');
-  const [guesses, setGuesses] = useState<string[]>([]);
-  const toastRef = useRef<ToastRef | null>(null);
+	const [word, setWord] = useState(getRandomWord(words));
+	const [currentGuess, setCurrentGuess] = useState("");
+	const [guesses, setGuesses] = useState<string[]>([]);
+	const toastRef = useRef<ToastRef | null>(null);
 
-  const status: GameStatus = useMemo(() => {
-    const hasWon = guesses.some((guess) => guess === word);
+	const status: GameStatus = useMemo(() => {
+		const hasWon = guesses.some((guess) => guess === word);
 
-    if (hasWon) return 'won';
+		if (hasWon) return "won";
 
-    if (guesses.length === AMOUNT_OF_GUESSES) return 'lost';
+		if (guesses.length === AMOUNT_OF_GUESSES) return "lost";
 
-    return 'playing';
-  }, [word, guesses]);
+		return "playing";
+	}, [word, guesses]);
 
-  const handleGameReset = useCallback(() => {
-    // todo reset game status
-    setWord(getRandomWord(words));
-    setGuesses([]);
-  }, []);
+	const handleGameReset = useCallback(() => {
+		// todo reset game status
+		setWord(getRandomWord(words));
+		setGuesses([]);
+	}, []);
 
-  const handleSubmitGuess: React.FormEventHandler<HTMLFormElement> =
-    useCallback(
-      (e) => {
-        e.preventDefault();
+	const handleSubmitGuess: React.FormEventHandler<HTMLFormElement> =
+		useCallback(
+			(e) => {
+				e.preventDefault();
 
-        if (currentGuess.length !== WORD_LENGTH) {
-          toastRef.current?.show(`guess must be ${WORD_LENGTH} letters long`);
-          return;
-        }
+				if (currentGuess.length !== WORD_LENGTH) {
+					toastRef.current?.show(`guess must be ${WORD_LENGTH} letters long`);
+					return;
+				}
 
-        setGuesses((prevGuesses) => [...prevGuesses, currentGuess]);
-      },
-      [currentGuess]
-    );
+				setGuesses((prevGuesses) => [...prevGuesses, currentGuess]);
+			},
+			[currentGuess]
+		);
 
-  const handleCurrentGuessChange: React.ChangeEventHandler<HTMLInputElement> =
-    useCallback((e) => {
-      const guess = e.currentTarget.value;
-      if (guess.length > 5) {
-        return;
-      }
+	const handleCurrentGuessChange: React.ChangeEventHandler<HTMLInputElement> =
+		useCallback((e) => {
+			const guess = e.currentTarget.value;
+			if (guess.length > 5) {
+				return;
+			}
 
-      setCurrentGuess(guess);
-    }, []);
+			setCurrentGuess(guess);
+		}, []);
 
-  return (
-    <section className='pt-4 max-w-lg mx-auto flex flex-col gap-4'>
-      <Guesses guesses={guesses} />
-      {status === 'playing' && (
-        <div className='flex flex-col gap-4'>
-          <form onSubmit={handleSubmitGuess}>
-            <fieldset className='flex gap-4'>
-              <legend className='sr-only'>Make your guesses here!</legend>
-              <label htmlFor='guess'>Enter a guess:</label>
-              <input
-                className='text-black'
-                name='guess'
-                id='guess'
-                maxLength={5}
-                type='text'
-                onChange={handleCurrentGuessChange}
-              />
-            </fieldset>
-          </form>
-        </div>
-      )}
-      {status === 'won' && <div className='flex flex-col gap-4'>You won!</div>}
-      {status === 'lost' && (
-        <div className='flex flex-col gap-4'>You lost! The word was {word}</div>
-      )}
-      <button onClick={handleGameReset}>Reset Game</button>
-      <Toast ref={toastRef} />
-    </section>
-  );
+	return (
+		<section className="pt-4 max-w-lg mx-auto flex flex-col gap-4">
+			<Guesses guesses={guesses} />
+			{status === "playing" && (
+				<div className="flex flex-col gap-4">
+					<form onSubmit={handleSubmitGuess}>
+						<fieldset className="flex gap-4">
+							<legend className="sr-only">Make your guesses here!</legend>
+							<label htmlFor="guess">Enter a guess:</label>
+							<input
+								className="text-black"
+								name="guess"
+								id="guess"
+								maxLength={5}
+								type="text"
+								onChange={handleCurrentGuessChange}
+							/>
+						</fieldset>
+					</form>
+				</div>
+			)}
+			{status === "won" && <div className="flex flex-col gap-4">You won!</div>}
+			{status === "lost" && (
+				<div className="flex flex-col gap-4">You lost! The word was {word}</div>
+			)}
+			<button onClick={handleGameReset}>Reset Game</button>
+			<Toast ref={toastRef} />
+		</section>
+	);
 }
 
 function Guesses({ guesses }: { guesses: string[] }) {
-  return [
-    ...Array.from({ length: AMOUNT_OF_GUESSES }, (_, index) => index + 1),
-  ].map((num: number, i) => {
-    return <Guess key={num} guess={guesses[i]} />;
-  });
+	return [
+		...Array.from({ length: AMOUNT_OF_GUESSES }, (_, index) => index + 1),
+	].map((num: number, i) => {
+		return <Guess key={num} guess={guesses[i]} />;
+	});
 }
 
 export default App;
--- src/components/Guess/Guess.tsx
+++ src/components/Guess/Guess.tsx
@@ -1,20 +1,59 @@
+import classNames from "classnames";
 import { WORD_LENGTH } from "../../App";
 
-export function Guess({ guess = "" }: { guess: string }) {
+// LetterMatch?
+type LetterStatus = "MISPLACED" | "CORRECT" | "INCORRECT" | "COVERED";
+
+const LetterStatusStyleMap: Record<LetterStatus, string> = {
+	CORRECT: "bg-lime-300",
+	COVERED: "bg-transparent",
+	INCORRECT: "bg-red-500",
+	MISPLACED: "bg-amber-400 ",
+};
+
+function GuessLetter({
+	letter,
+	letterStatus,
+}: {
+	letter: string;
+	letterStatus: LetterStatus;
+}) {
 	return (
-		<div className="flex justify-center gap-4">
-			{[...Array.from({ length: WORD_LENGTH }, (_, index) => index + 1)].map(
-				(_: number, i) => {
-					return (
-						<div
-							key={i}
-							className="inline-block text-center text-2xl font-bold py-2 px-2 size-14 border-2 border-white"
-						>
-							<span className="align-middle">{guess.charAt(i)}</span>
-						</div>
-					);
-				}
-			)}
+		<div
+			className={classNames("guess-letter", LetterStatusStyleMap[letterStatus])}
+		>
+			<span className="align-middle">{letter}</span>
 		</div>
 	);
+}
+
+export function Guess({
+	guess,
+	getLetterStatusInWord,
+}: {
+	guess: string;
+	getLetterStatusInWord(letter: string): LetterStatus;
+}) {
+	let content;
+	if (!guess) {
+		content = [
+			...Array.from({ length: WORD_LENGTH }, (_, index) => index + 1),
+		].map((_: number, i) => {
+			return (
+				<GuessLetter letter="" key={i} letterStatus="COVERED"></GuessLetter>
+			);
+		});
+	} else {
+		content = guess.split("").map((letter: string, i) => {
+			return (
+				<GuessLetter
+					key={i}
+					letter={letter}
+					letterStatus={getLetterStatusInWord(letter)}
+				/>
+			);
+		});
+	}
+
+	return <div className="flex justify-center gap-4">{content}</div>;
 }
