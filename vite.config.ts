90b68af482c7e86c8abf46873649791cc30b1371
Denoph <gaspar.arimany@gmail.com>
2025-01-29 22:10:18 -0300

    chore: adds base property to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ const ReactCompilerConfig = {};
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react({ babel: { plugins: ["babel-plugin-react-compiler", ReactCompilerConfig] } })],
+	base: "./",
 });
